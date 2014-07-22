2f04e0ec52a858708785e462977e074a01efbc8c
moof mayeda & Scott Dudley <student@epicodus.com>
2014-07-22 15:01:17 -0700

    identifies all poker hands

--- lib/poker_hand.rb
+++ lib/poker_hand.rb
@@ -1,35 +1,72 @@
 def poker_hand(hand)
-
+  numbers = [hand[0][0..-2].to_i, hand[1][0..-2].to_i, hand[2][0..-2].to_i, hand[3][0..-2].to_i, hand[4][0..-2].to_i]
+  counts = Hash.new 0
+  numbers.each do |number|
+    counts[number] += 1
+  end
+  if is_flush(hand) && is_straight(hand)
+    return "straight flush"
+  elsif is_three_of_kind(counts) && is_pair(counts)
+    return "full house"
+  elsif is_four_of_kind(counts)
+    return "four of a kind"
+  elsif is_three_of_kind(counts)
+    return "three of a kind"
+  elsif is_pair(counts)
+    return "a pair"
+  elsif is_two_pair(counts)
+    return "two pair"
+  elsif is_straight(hand)
+    return "straight"
+  elsif is_flush(hand)
+    return "flush"
+  else
+    return "your hand sucks"
+  end
 end
 
 def is_flush(hand)
   suits = [hand[0][-1], hand[1][-1], hand[2][-1], hand[3][-1], hand[4][-1]]
-  if suits.uniq.length == 1
-    return true
-  else
-    return false
-  end
+  suits.uniq.length == 1 ? true : false
 end
 
 def is_straight(hand)
   numbers = [hand[0][0..-2].to_i, hand[1][0..-2].to_i, hand[2][0..-2].to_i, hand[3][0..-2].to_i, hand[4][0..-2].to_i]
 
-  # ASK WHY THIS DOESN'T WORK
-  # numbers.each do |number|
+  # ASK WHY THIS DOESN'T WORK - Mac solved it below.  Needed another argument
+  # numbers.each do |number, index|
   #   number = number.to_i
+  #    number[index] = number.to_i
   # end
 
   numbers.sort!
   numbers.each_cons(2).all? {|x,y| y == x + 1}
 end
 
-def is_three_or_two_pair(hand)
-  numbers = [hand[0][0..-2].to_i, hand[1][0..-2].to_i, hand[2][0..-2].to_i, hand[3][0..-2].to_i, hand[4][0..-2].to_i]
-  # numbers.uniq.length == 4 ? true : false
-  numbers.uniq.length == 3 ? true : false
+def is_four_of_kind(counts)
+  counts.detect {|key, count| count == 4} != nil
 end
 
-def is_pair(hand)
-  numbers = [hand[0][0..-2].to_i, hand[1][0..-2].to_i, hand[2][0..-2].to_i, hand[3][0..-2].to_i, hand[4][0..-2].to_i]
-  numbers.uniq.length == 4 ? true : false
+def is_three_of_kind(counts)
+  counts.detect {|key, count| count == 3} != nil
+end
+
+def is_pair(counts)
+  result = false
+  counts.each do |key, count|
+    if count == 2
+      result = !result
+    end
+  end
+  return result
+end
+
+def is_two_pair(counts)
+  twos = 0
+  counts.each do |key, count|
+    if count == 2
+      twos += 1
+    end
+  end
+  twos == 2 ? true : false
 end
--- spec/poker_hand_spec.rb
+++ spec/poker_hand_spec.rb
@@ -23,14 +23,53 @@ end
 
 describe("is_pair") do
   it("checks if a hand has a pair") do
-    is_pair(["2H", "2S", "9D", "5H", "6C"]).should(eq(true))
+    is_pair({13 => 2, 5 => 1, 7 => 1, 8 => 1}).should(eq(true))
   end
   it("checks if a had does not have any duplicate numbers") do
-    is_pair(["13H", "2S", "4D", "5H", "6C"]).should(eq(false))
+    is_pair({13 => 2, 5 => 1, 7 => 2}).should(eq(false))
   end
 end
-describe("is_three_or_two_pair") do
+
+describe("is_two_pair") do
+  it("checks if a hand has two pairs") do
+    is_two_pair({13 => 2, 5 => 2, 7 => 1}).should(eq(true))
+  end
+end
+
+describe("is_three_of_kind") do
   it("checks if a hand has a 3 of a kind") do
-    is_three_or_two_pair(["13H", "13S", "13D", "5H", "6C"]).should(eq(true))
+    is_three_of_kind({13 => 3, 5 => 1, 7 => 1}).should(eq(true))
+  end
+
+  it("checks if a hand is not a 3 of a kind") do
+    is_three_of_kind({13 => 2, 5 => 1, 7 => 1, 8 => 1}).should(eq(false))
+  end
+end
+
+describe("poker_hand") do
+  it("takes a three of a kind and returns its type") do
+    poker_hand(["13H", "13S", "13D", "5H", "6C"]).should(eq("three of a kind"))
+  end
+
+  it("takes a straight flush and returns its type ") do
+    poker_hand(["2H", "3H", "4H", "5H", "6H"]).should(eq("straight flush"))
+  end
+
+  it("takes a full house and returns its type") do
+    poker_hand(["2H", "2D", "2C", "5H", "5C"]).should(eq("full house"))
+  end
+
+  it("takes a crappy hand and tells you it is crap") do
+    poker_hand(["2H", "4D", "8C", "10H", "6H"]).should(eq("your hand sucks"))
+  end
+end
+
+describe("is_four_of_kind") do
+  it("checks if a hand has four of a kind") do
+    is_four_of_kind({13 => 4, 5 => 1}).should(eq(true))
+  end
+
+  it("checks if a hand does not have four of a kind") do
+    is_four_of_kind({13 => 3, 5 => 1, 2 => 1}).should(eq(false))
   end
 end
